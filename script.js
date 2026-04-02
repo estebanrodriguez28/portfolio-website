63a78dd982d7a056d8816264076089bc218265c2
Esteban Rodriguez <estebanrodriguez@sandiego.edu>
2026-04-01 19:13:29 -0700

    Made dropdown menu in footer clickable with javascript

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
-const footer_dropdown = document.getElementById("footer-dropdown");
-console.log(footer_dropdown)
-footer_dropdown.onclick = function () {
-    document.getElementsByClassName('dropdown-content').style.display = "block"
+
+function open_dropdown() {
+    dropdown_state = document.getElementsByClassName('dropdown-content')[0].style.display;
+    if (!dropdown_state) {
+        document.getElementsByClassName('dropdown-content')[0].style.display = "block";
+    }
+    else {
+        document.getElementsByClassName('dropdown-content')[0].style.display = "";
+    }
 };
