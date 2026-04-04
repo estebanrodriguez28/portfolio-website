6b166090d8418ae5c8b00aae9877aaa80feb4f11
Esteban Rodriguez <estebanrodriguez@sandiego.edu>
2026-04-04 16:39:50 -0700

    Migrated to jquery, added fade animations for the navbars on mobile

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 
 
-
-
-
-
-
 function open_dropdown() {
-    let dropdown_state = document.getElementsByClassName('dropdown-content')[0].style.display;
-    if (!dropdown_state) {
-        document.getElementsByClassName('dropdown-content')[0].style.display = "block";
+    $(document).ready(function () {
+        let dropdown_state = $(".dropdown-content").css("display");
+        console.log(dropdown_state);
+        if (dropdown_state == "none") {
+            $(".dropdown-content").css("display", "block");
+        }
+
+        else {
+            $(".dropdown-content").css("display", "");
+        }
+
+
     }
-    else {
-        document.getElementsByClassName('dropdown-content')[0].style.display = "";
-    }
+    )
+
 };
 
 function open_mobile_menu() {
-    document.getElementById('navbar').classList.add("fade-out");
 
-    document.getElementById('navbar').style.display = "none";
-    document.getElementById('intro').style.minHeight = "100dvh";
-    document.getElementById('intro').style.paddingBottom = "5dvh";
-    document.getElementsByClassName('mobile-menu-popup')[0].style.display = "flex";
-
-    document.querySelectorAll(".mobile-menu-popup-nav ol li").forEach(list_item => {
-
-        list_item.addEventListener("click", close_mobile_popup);
-
-    }
-    );
-
+    $(document).ready(function () {
+        $("#navbar").fadeOut(80, function () {
+            $("#intro").css("minHeight", "100dvh");
+            $(".mobile-menu-popup").css("display", "flex");
+            $(".mobile-menu-popup-nav ol li").click(close_mobile_popup);
+        })
+    });
 
 
 }
 
 function close_mobile_popup() {
-    document.getElementsByClassName('mobile-menu-popup')[0].style.display = "";
-    document.getElementById('navbar').classList.add("fade-in");
-    document.getElementById('navbar').style.display = "flex";
-    document.getElementById('intro').style.minHeight = "80dvh";
-    document.getElementById('intro').style.paddingBottom = "0dvh";
 
+    $(document).ready(function () {
+        $(".mobile-menu-popup").css("display", "");
+        $("#navbar").fadeIn(500);
+        $("#navbar").css("display", "flex");
+        $("#intro").css("minHeight", "80dvh");
+    })
 
 }
