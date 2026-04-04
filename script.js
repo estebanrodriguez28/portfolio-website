e2fe0f7aa0a011bd3edc7e447616868d2b19b2e3
Esteban Rodriguez <estebanrodriguez@sandiego.edu>
2026-04-03 17:53:38 -0700

    Added the mobile navigation popup when the hamburger is clicked. The links scroll to the sections of the portfolio on desktop + mobile.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 function open_dropdown() {
-    dropdown_state = document.getElementsByClassName('dropdown-content')[0].style.display;
+    let dropdown_state = document.getElementsByClassName('dropdown-content')[0].style.display;
     if (!dropdown_state) {
         document.getElementsByClassName('dropdown-content')[0].style.display = "block";
     }
@@ -9,3 +9,20 @@ function open_dropdown() {
         document.getElementsByClassName('dropdown-content')[0].style.display = "";
     }
 };
+
+function open_mobile_menu() {
+    document.getElementById('navbar').style.display = "none";
+    document.getElementById('intro').style.minHeight = "100dvh";
+    document.getElementById('intro').style.paddingBottom = "5dvh";
+    document.getElementsByClassName('mobile-menu-popup')[0].style.display = "flex";
+
+}
+
+function close_mobile_popup() {
+    document.getElementsByClassName('mobile-menu-popup')[0].style.display = "";
+    document.getElementById('navbar').style.display = "flex";
+    document.getElementById('intro').style.minHeight = "80dvh";
+    document.getElementById('intro').style.paddingBottom = "0dvh";
+
+
+}
