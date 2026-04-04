14be7c3790bbf7f29eb084562ad85d188265b588
Esteban Rodriguez <estebanrodriguez@sandiego.edu>
2026-04-04 09:59:19 -0700

    Added link to resume button in nav for desktop + mobile. Made each nav link in mobile popup nav menu close the popup nav menu so at ttop of page old top nav restored with letter e logo on left + hamburger to right. Added a fade in animation for the mobile popup menu. Added a fade in animation for when the original top nav comes back, working on the fade out animation.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 
+
+
+
+
+
 function open_dropdown() {
     let dropdown_state = document.getElementsByClassName('dropdown-content')[0].style.display;
     if (!dropdown_state) {
@@ -11,15 +16,27 @@ function open_dropdown() {
 };
 
 function open_mobile_menu() {
+    document.getElementById('navbar').classList.add("fade-out");
+
     document.getElementById('navbar').style.display = "none";
     document.getElementById('intro').style.minHeight = "100dvh";
     document.getElementById('intro').style.paddingBottom = "5dvh";
     document.getElementsByClassName('mobile-menu-popup')[0].style.display = "flex";
 
+    document.querySelectorAll(".mobile-menu-popup-nav ol li").forEach(list_item => {
+
+        list_item.addEventListener("click", close_mobile_popup);
+
+    }
+    );
+
+
+
 }
 
 function close_mobile_popup() {
     document.getElementsByClassName('mobile-menu-popup')[0].style.display = "";
+    document.getElementById('navbar').classList.add("fade-in");
     document.getElementById('navbar').style.display = "flex";
     document.getElementById('intro').style.minHeight = "80dvh";
     document.getElementById('intro').style.paddingBottom = "0dvh";
