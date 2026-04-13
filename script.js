0826591013016190e92ca879ab2d43fc281f8797
Esteban Rodriguez <estebanrodriguez@sandiego.edu>
2026-04-13 12:47:01 -0700

    Made the nav links on desktop have an underline that slides in when user hovers over a link

--- script.js
+++ script.js
@@ -2,71 +2,85 @@
 // Posted by Josiah Ruddell, modified by community. See post 'Timeline' for change history
 // Retrieved 2026-04-09, License - CC BY-SA 3.0
 
-var lastScrollTop = 0;
-$(window).scroll(function (event) {
-    //$("#navbar").css("box-shadow", "none");
-    // $("#navbar").addClass("box-shadow-fadeout");
-    var st = $(this).scrollTop();
-    if (st > 50) {
-        $("#navbar").css("box-shadow", "0px 4px 12px -1px rgba(0, 0, 0, 0.3)");
-        if (st > lastScrollTop) {
-            // downscroll code
-            $("#navbar").slideUp("fast");
-        } else {
-            // upscroll code
-            $("#navbar").slideDown("fast");
-        }
-        lastScrollTop = st;
-    }
 
-});
+
+function nav_scroll() {
+    var lastScrollTop = 0;
+    $(window).scroll(function (event) {
+        // $("#navbar").css("box-shadow", "none");
+        // $("#navbar").addClass("box-shadow-fadeout");
+        var st = $(this).scrollTop();
+        if (st > 50) {
+            // $("#navbar").css("box-shadow", "0px 4px 12px -1px rgba(0, 0, 0, 0.3)");
+            if (st > lastScrollTop) {
+                // downscroll code
+                $("#navbar").slideUp("fast");
+            } else {
+                // upscroll code
+                $("#navbar").slideDown("fast");
+            }
+            lastScrollTop = st;
+        }
+
+    });
+}
+
+
+function nav_link_underline() {
+    $("#desktop-nav a").hover(
+        function () {
+            var link_width = $(this).width();
+            document.documentElement.style.setProperty("--underline-width-hover", `${link_width}px`);
+        }
+    );
+}
 
 
 
 
 function open_dropdown() {
-    $(document).ready(function () {
-        let dropdown_state = $(".dropdown-content").css("display");
-        console.log(dropdown_state);
-        if (dropdown_state == "none") {
-            $(".dropdown-content").css("display", "block");
-        }
-
-        else {
-            $(".dropdown-content").css("display", "");
-        }
-
-
+    let dropdown_state = $(".dropdown-content").css("display");
+    if (dropdown_state == "none") {
+        $(".dropdown-content").css("display", "block");
     }
-    )
+
+    else {
+        $(".dropdown-content").css("display", "");
+    }
+
+
+
 
 };
 
 function open_mobile_menu() {
 
-    $(document).ready(function () {
-        $("#navbar").fadeOut(80, function () {
-            //$("#intro").css("minHeight", "100dvh");
-            //$("#intro").css("filter", "blur(6px)");
-            $(".mobile-menu-popup").css("display", "flex");
-            $(".mobile-menu-popup-nav ol li").click(close_mobile_popup);
-            $("body").css("overflow", "hidden");
+
+    $("#navbar").fadeOut(80, function () {
+        $(".mobile-menu-popup").css("display", "flex");
+        $(".mobile-menu-popup-nav ol li").click(close_mobile_popup);
+        $("body").css("overflow", "hidden");
 
 
-        })
-    });
+    })
+
 
 
 }
 
 function close_mobile_popup() {
+    $(".mobile-menu-popup").css("display", "");
+    $("#navbar").fadeIn(300);
+    $("#navbar").css("display", "flex");
+    //$("#intro").css("minHeight", "80dvh");
+    $("body").css("overflow", "visible");
 
-    $(document).ready(function () {
-        $(".mobile-menu-popup").css("display", "");
-        $("#navbar").fadeIn(300);
-        $("#navbar").css("display", "flex");
-        //$("#intro").css("minHeight", "80dvh");
-        $("body").css("overflow", "visible");
-    })
 
 }
+
+
+
+$(document).ready(function () {
+    nav_link_underline();
+});
+
